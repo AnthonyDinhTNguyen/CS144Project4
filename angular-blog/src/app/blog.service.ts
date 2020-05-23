e515e5d51c452d6f1a2e0257becdb35e07a52a6e
Anthony Nguyen <anthonydtnguyen@gmail.com>
2020-05-23 16:43:45 -0700

    edit

--- angular-blog/src/app/blog.service.ts
+++ angular-blog/src/app/blog.service.ts
@@ -4,7 +4,8 @@ import { Injectable } from '@angular/core';
   providedIn: 'root'
 })
 export class BlogService {
-  //posts: Post[] = [];
+  posts: Post[] = [];
+  draft: Post = null;
   constructor() { }
 
   fetchPosts(username:string):Promise<Post[]>{
@@ -13,9 +14,9 @@ export class BlogService {
       credentials:'include'
     }).then(response => response.json()).catch((err)=>{console.log(err); Promise.reject(err)})
     .then(response => {     
-      let posts:Post[] = []
       response.forEach(k => {
-        posts.push({
+        this.posts = [];
+        this.posts.push({
           postid: k.postid,
           created: new Date(k.created),
           modified: new Date(k.modified),
@@ -23,29 +24,48 @@ export class BlogService {
           body: k.body
         });
       });
-      return posts;
+      return this.posts;
     });
   }
-  // getPost():Post[]{
-  //   return this.posts;
-  // }
-    
-  //   .then((response:any)=>{response.json()}).then((response:any)=>{
-  //     let posts: Post[] = []
-  //     response.forEach(k=>{
-  //       posts.push({
-  //         postid: k.postid,
-  //         created: new Date(k.created),
-  //         modified: new Date(k.modified),
-  //         title: k.title,
-  //         body: k.body
-  //       })
-  //     })
-  //     return posts;
-  //   })
-  // }
+  getUsername(){
+    let cookie = document.cookie;
+    if (cookie == null){
+      return null;
+    }
+    else{
+      let jwtToken = parseJWT(cookie);
+      let username = jwtToken.usr;
+      return username;
+    }
+  }
+  getPost(username:string, id:number):Promise<Post>{
+    return fetch('/api/'+username+"/"+id.toString(),{
+      method:"GET",
+      credentials:'include'
+    }).then(response=>response.json()).then(response=>{
+      let post:Post = {
+        postid: response.postid,
+        created: new Date(response.created),
+        modified: new Date(response.modified),
+        title: response.title,
+        body: response.body
+      }
+      return post;
+    })
+  }
+  setCurrentDraft(draft:Post){
+    this.draft = draft;
+  }
+  getCurrentDraft(){
+    return this.draft;
+  }
 }
-
+function parseJWT(token) //got this from the spec
+{
+    let base64Url = token.split('.')[1];
+    let base64 = base64Url.replace(/-/g, '+').replace(/_/g, '/');
+    return JSON.parse(atob(base64));
+}
 export class Post {
   postid: number;
   created: Date;
--- angular-blog/src/app/edit/edit.component.ts
+++ angular-blog/src/app/edit/edit.component.ts
@@ -1,15 +1,41 @@
 import { Component, OnInit } from '@angular/core';
-
+import {Post,BlogService} from '../blog.service';
+import { Routes, RouterModule } from '@angular/router';
+import {Router, ActivatedRoute} from '@angular/router';
 @Component({
   selector: 'app-edit',
   templateUrl: './edit.component.html',
   styleUrls: ['./edit.component.css']
 })
 export class EditComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit(): void {
+  post: Post;
+  blogService: BlogService;
+  router: Router;
+  activatedRoute: ActivatedRoute;
+  constructor(blogService:BlogService, router:Router, activatedRoute:ActivatedRoute) {
+    this.blogService = blogService;
+    this.router = router;
+    this.activatedRoute = activatedRoute;
   }
 
+  ngOnInit(): void {
+    this.activatedRoute.paramMap.subscribe(()=>this.initializePost());
+    this.initializePost();
+  }
+  initializePost(){
+    //let id = this.activatedRoute.snapshot.paramMap.get('id');
+    //id = parseInt(id);
+    let id = 1;
+    let user = this.blogService.getUsername();
+    if(user == null){
+      //logout
+    }
+    else{
+      //Error CHeck id for nan
+      this.post = this.blogService.getCurrentDraft();
+      if(this.post.postid!= id){
+        this.blogService.getPost(user, id).then(result => this.post = result);
+      }
+    }
+  }
 }
